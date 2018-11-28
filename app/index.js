a3ccee5ded4556c3ebfed8b602e8934f6f05bb6f
Toriq Zainuddin <toriqz@gmail.com>
2018-11-28 17:33:51 +0800

    remove hardcoded test for CRUD

--- app/index.js
+++ app/index.js
@@ -9,13 +9,6 @@ var url = require("url");
 var StringDecoder = require("string_decoder").StringDecoder;
 var config = require("./config");
 var fs = require("fs");
-var _data = require('./lib/data');
-
-// TESTING
-// @TODO delete this
-_data.delete('test', 'newFile', function(err) {
-  console.log('this was the error', err);
-});
 
 // Instantiating HTTP server
 var httpServer = http.createServer(function (req,res) {
