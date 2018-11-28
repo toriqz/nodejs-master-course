2c6a8bc4f09221233ca0ec7bf897f825847686b7
Toriq Zainuddin <toriqz@gmail.com>
2018-11-28 16:46:44 +0800

    replace create test with read test

--- app/index.js
+++ app/index.js
@@ -13,8 +13,8 @@ var _data = require('./lib/data');
 
 // TESTING
 // @TODO delete this
-_data.create('test', 'newFile', {'foo' : 'bar'}, function(err) {
-  console.log('this was the error', err);
+_data.read('test', 'newFile', function(err, data) {
+  console.log('this was the error', err, 'and this was the data ', data);
 });
 
 // Instantiating HTTP server
